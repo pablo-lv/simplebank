31ba038f354a0218bf980a40e98952647096df5e
pablo <pablo.lucv@gmail.com>
2022-07-01 13:46:12 -0500

    Fix tests.

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"simplebank/util"
 	"testing"
 	"time"
@@ -73,8 +74,8 @@ func TestDeleteAccount(t *testing.T) {
 	require.NoError(t, err)
 
 	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
-	require.NoError(t, err)
-	// require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
 	require.Empty(t, account2)
 }
 

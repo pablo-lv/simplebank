14fb07a62e1cab9fa8fd32b2ea893d1144b18dc6
pablo <pablo.lucv@gmail.com>
2022-07-06 15:50:46 -0500

    Added create transfer endpoint and fix some typos

--- util/config.go
+++ util/config.go
@@ -5,7 +5,7 @@ import "github.com/spf13/viper"
 type Config struct {
 	DBDriver      string `mapstructure:"DB_DRIVER"`
 	DBSource      string `mapstructure:"DB_SOURCE"`
-	ServerAddress string `mapstructure:"DB_ADDRESS"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
 }
 
 func LoadConfig(path string) (config Config, err error) {

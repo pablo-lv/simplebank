45b65514fbfda3ac58a9d1372394747ca680a116
pablo <pablo.lucv@gmail.com>
2022-07-08 14:41:04 -0500

    Fixed user sql issues

--- api/validator.go
+++ api/validator.go
@@ -5,8 +5,8 @@ import (
 	"simplebank/util"
 )
 
-var validCurrency validator.Func = func(fl validator.FieldLevel) bool {
-	if currency, ok := fl.Field().Interface().(string); ok {
+var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
+	if currency, ok := fieldLevel.Field().Interface().(string); ok {
 		return util.IsSupportedCurrency(currency)
 	}
 	return false
--- db/query/user.sql
+++ db/query/user.sql
@@ -1,11 +1,13 @@
 -- name: CreateUser :one
-INSERT INTO users (username,
-                   hashed_password,
-                   full_name,
-                   email)
-VALUES ($1, $2, $3, $4) RETURNING *;
+INSERT INTO users (
+    username,
+    hashed_password,
+    full_name,
+    email
+) VALUES (
+             $1, $2, $3, $4
+         ) RETURNING *;
 
 -- name: GetUser :one
-SELECT *
-FROM users
+SELECT * FROM users
 WHERE username = $1 LIMIT 1;
